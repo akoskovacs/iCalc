2771133d2dbbf9a5403817dd7a51c6a46ad6b1b9
Ákos Kovács <akoskovacs0@gmail.com>
2024-11-30 03:35:52 +0100

    Initial Commit

---
+++ iCalc/iCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalcApp.swift
+//  iCalc
+//
+//  Created by Ákos Kovács on 2024. 11. 30..
+//
+
+import SwiftUI
+
+@main
+struct iCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iCalcTests/iCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  iCalcTests.swift
+//  iCalcTests
+//
+//  Created by Ákos Kovács on 2024. 11. 30..
+//
+
+import Testing
+@testable import iCalc
+
+struct iCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
